6c1b3bc8d6a7c9bb2cad6eac0f24a89dd3a02a73
Brian Zalewski <brian@megabyte.space>
2021-06-17 02:01:28 +0000

    Update scripts/desktop.custom.sh

--- scripts/desktop.custom.sh
+++ scripts/desktop.custom.sh
@@ -1,10 +1,13 @@
 #!/bin/sh -eux
 
 echo "Install the tasksel manager utility"
-sudo apt-get install -y tasksel
+sudo apt install -y tasksel
 
 echo "Install minimal Desktop environment"
 sudo apt install -y ubuntu-desktop --no-install-recommends
+
+echo "Install Plymouth"
+sudo apt install -y plymouth plymouth-themes
 
 echo "Ensure Grub has quiet splash setting"
 sudo sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"' /etc/default/grub
